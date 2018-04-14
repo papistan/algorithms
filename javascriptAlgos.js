6c0b67447c39bcaff9570852abd0d408c60c04f6
Jay Papisan <jpapisan@gmail.com>
2018-04-14 15:46:22 -0700

    Update javascriptAlgos.js

--- javascriptAlgos.js
+++ javascriptAlgos.js
@@ -498,3 +498,16 @@ function isPalindrome(string) {
 }
 
 // end 
+
+//  implement indexOf function 
+function indexOf(array, num) {
+  
+  for(let i=0;i<array.length;i++) {
+    if (array[i] == num) {
+      return i
+    }
+  }
+  return -1
+}
+//  end 
+
