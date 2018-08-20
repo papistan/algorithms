caffd022a2178e61576087c424813193777bfe12
Jay Papisan <jpapisan@gmail.com>
2018-08-20 15:19:12 -0700

    Update javascriptAlgos.js

--- javascriptAlgos.js
+++ javascriptAlgos.js
@@ -1,37 +1,4 @@
-// random element returner 
-const randomElementGenerator = (myArray, m) => {
-  const elements = {}
-  const length = myArray.length - 1
-  while(Object.keys(elements).length < m){
-    const randomNum = Math.floor((Math.random() * length) + 1)
-    if(!elements[randomNum]){
-      elements[randomNum] = myArray[randomNum]
-    }
-  }
-  return Object.keys(elements).map(num => elements[num])
-}
-// end
 
-
-// random num generator 
- const randomNumGenerator = (n, m) => {
-  	const numbers = {}
-  	while(Object.keys(numbers).length < m){
-    	  const randomNum = Math.floor((Math.random() * n) + 1)
-    	  if(!numbers[randomNum]){
-      	    numbers[randomNum] = true
-    	  }
-  	}
-  	return Object.keys(numbers).map(num => parseInt(num))
-      }
- 
- // end 
-
-
-// reverse data
-const reverse = (data) => data.reduceRight((a, b) => a.concat(b), [])
-
-// end
 
 // Find elements in both arrays equal to sum
 const sumToNum = (array1, array2, sum) => {
